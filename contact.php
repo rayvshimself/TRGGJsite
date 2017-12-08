c1bb78abdd9b73410265ccaade22e0e7c74736dd
hollowspecter <hollowspecder@googlemail.com>
2017-12-08 11:27:28 +0100

    fixed contact form with recaptcha

--- contact.php
+++ contact.php
@@ -1,7 +1,7 @@
 <?php
     $title = "GGJ Trier - Contact Form";
     $selectedMenuItem = "contact";
-    // $script = "<script src='https://www.google.com/recaptcha/api.js'></script>";
+    $script = "";
     include "html/header.php";
     include "html/contact.html";
     include "html/footer.html";
